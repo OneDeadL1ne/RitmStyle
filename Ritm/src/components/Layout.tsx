843d43587743e9594bd80329635623c99a33fba4
OneDeadL1ne <119927472+OneDeadL1ne@users.noreply.github.com>
2024-03-21 11:47:04 +0300

    fix errors console

--- Ritm/src/components/Layout.tsx
+++ Ritm/src/components/Layout.tsx
@@ -4,18 +4,16 @@ import Footer from "./Footer";
 
 export default function Layout() {
 	return (
-		<html lang="en">
-			<body className="flex flex-col h-screen">
-				<header className=" w-full">
-					<Header />
-				</header>
-				<main className="min-h-full flex-grow">
-					<Outlet />
-				</main>
-				<footer className="flex w-full justify-center row-auto  text-white  ">
-					<Footer />
-				</footer>
-			</body>
-		</html>
+		<div className="flex flex-col h-screen">
+			<header className=" w-full">
+				<Header />
+			</header>
+			<main className="min-h-full flex-grow">
+				<Outlet />
+			</main>
+			<footer className="flex w-full justify-center row-auto  text-white  ">
+				<Footer />
+			</footer>
+		</div>
 	);
 }
